3468ac8d852b64c50732637b0efc07088f4cd2fd
unknown <6699228@gmail.com>
2021-11-03 18:59:36 +0300

    Репозиторий с автотестом по заданию Ивана

---
+++ cypress/integration/BBC.spec.js
@@ -0,0 +1,25 @@
+
+describe('Найти элементы по заданию Ивана', () => {
+    it('Найти поле поиска ', () => {
+        cy.visit('https://www.bbc.com/')
+        cy.get('#orb-search-q').should('be.visible')
+    })
+    it('Найти кнопку погода ', () => {
+        cy.visit('https://www.bbc.com/')
+        cy.get('.orb-panel-content > ul > .orb-nav-weather > a').should('exist')
+    })
+    it('Найти первый бокс из блока новостей ', () => {
+        cy.visit('https://www.bbc.com/')
+        cy.get('.module--promo > .module__content > .media-list > .media-list__item--1 > .media > .block-link__overlay-link').should('exist')
+    })
+    it('Нати чётные элементы верхнего меню, которые не скрыты в More ', () => {
+        cy.visit('https://www.bbc.com/')
+        cy.get('#orb-nav-links > ul > .orb-nav-sport > a').should('exist')
+        cy.get('#orb-nav-links > ul > .orb-nav-worklife > a').should('exist')
+        cy.get('#orb-nav-links > ul > .orb-nav-future > a').should('exist')
+        cy.get('#orb-nav-links > ul > .orb-nav-music > a').should('exist')
+        cy.stop
+
+
+    })
+  })
