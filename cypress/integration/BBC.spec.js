0a9291727d4612874ce1cf2b9f54467dc0c0ca03
Denis Ponano <6699228@gmail.com>
2021-11-06 00:44:59 +0300

    Update BBC.spec.js
    ЗАМЕНИЛ ВАЛИДАЦИЮ ПРИСУТСТВИЯ ЭЛЕМЕНТОВ  В dom НА ПРОВЕРКУ ВИДИМОСТИ

--- cypress/integration/BBC.spec.js
+++ cypress/integration/BBC.spec.js
@@ -5,16 +5,17 @@ describe('Найти элементы по заданию Ивана', () => {
         cy.get('#orb-search-q').should('be.visible')
     })
     it('Найти кнопку погода ', () => {
-        cy.get('.orb-panel-content > ul > .orb-nav-weather > a').should('exist')
+        cy.get('.istats-notrack').click()
+        cy.get('.orb-panel-content > ul > .orb-nav-weather').should('be.visible')
     })
     it('Найти первый бокс из блока новостей ', () => {
-        cy.get('.module--promo > .module__content > .media-list > .media-list__item--1 > .media > .block-link__overlay-link').should('exist')
+        cy.get('.module--promo > .module__content > .media-list > .media-list__item--1 > .media > .block-link__overlay-link').should('be.visible')
     })
     it('Нати чётные элементы верхнего меню, которые не скрыты в More ', () => {
-        cy.get('#orb-nav-links > ul > .orb-nav-sport > a').should('exist')
-        cy.get('#orb-nav-links > ul > .orb-nav-worklife > a').should('exist')
-        cy.get('#orb-nav-links > ul > .orb-nav-future > a').should('exist')
-        cy.get('#orb-nav-links > ul > .orb-nav-music > a').should('exist')
+        cy.get('#orb-nav-links > ul > .orb-nav-sport > a').should('be.visible')
+        cy.get('#orb-nav-links > ul > .orb-nav-worklife > a').should('be.visible')
+        cy.get('#orb-nav-links > ul > .orb-nav-future > a').should('be.visible')
+        cy.get('#orb-nav-links > ul > .orb-nav-music > a').should('be.visible')
    
     })
   })
