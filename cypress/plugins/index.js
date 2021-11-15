a01e213ac5856e8df9e30626f6a40d5e410a190c
Denis Ponano <6699228@gmail.com>
2021-11-15 17:27:53 +0300

    понЕслось

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,24 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+// plugins/index.js
+
+// need to install these dependencies
+// npm i lodash del --save-dev
+const _ = require('lodash')
+const del = require('del')
+
+module.exports = (on, config) => {
+  on('after:spec', (spec, results) => {
+    if (results && results.video) {
+      // Do we have failures for any retry attempts?
+      const failures = _.some(results.tests, (test) => {
+        return _.some(test.attempts, { state: 'failed' })
+      })
+      if (!failures) {
+        // delete the video if the spec passed and no tests retried
+        return del(results.video)
+      }
+    }
+  })
+}
